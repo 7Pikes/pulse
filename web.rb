b43e191e9deb1c48c4a8109d7167089805aec83e
Dmitriy Kazantsev <d.kazantsev@7pikes.com>
2014-07-13 10:38:21 +0400

    Show deadline in ui

--- web.rb
+++ web.rb
@@ -2,6 +2,8 @@ require File.expand_path("../config/environment", __FILE__)
 
 require 'sinatra/base'
 
-server = ::Thin::Server.new("198.199.127.168", 3000, Frontend, {})
+# server = ::Thin::Server.new("198.199.127.168", 3000, Frontend, {})
+
+server = ::Thin::Server.new("localhost", 3000, Frontend, {})
 
 server.start
